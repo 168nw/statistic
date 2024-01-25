961f92d7d3bb8c637cab9becbb0e423a864dfec6
alexandr <168nwirk@gmail.com>
2024-01-25 16:50:10 +0800

    add pages form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import './scss/app.scss';
 import Header from './components/Header';
 import Start from './pages/Start';
+import FormPage from './pages/FormPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <div className="content">
         <Routes>
           <Route path="/" element={<Start />} />
+          <Route path="/form" element={<FormPage />} />
         </Routes>
       </div>
     </div>
---
+++ src/pages/FormPage.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import ActivityForm from '../components/Form.jsx';
+import { Link } from 'react-router-dom';
+
+
+const FormPage = () => {
+
+
+  return(
+    <div className="container">
+      <div className="content__top"> 
+      {/* Добавление чего то в хедер меню или модальное окно */}
+      </div>
+      <div className="content__items">
+        <h1>Заполните ваш типичный день </h1>
+      </div>
+      <ActivityForm />
+      <div className="content__items"> 
+        <Link to="/" class="button button--outline">
+        <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M7 13L1 6.93015L6.86175 1" stroke="#D3D3D3" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+        <span>Вернуться назад</span>
+        </Link>
+      </div> 
+    </div>
+      
+  )
+  }
+
+  export default FormPage
